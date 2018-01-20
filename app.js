64c0dbd969a31c3d6685d53edb91ef9dccbadd46
Mukesh Selvaraj <33991311+ssmukesh@users.noreply.github.com>
2018-01-21 03:59:29 +0530

    api updated
    Signed-off-by: Mukesh Selvaraj <33991311+ssmukesh@users.noreply.github.com>

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var QuickBooks = require('node-quickbooks');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var qbconnect = require('./routes/qbconnect');
 
 var app = express();
 
@@ -14,33 +15,23 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.set('appCenter', QuickBooks.APP_CENTER_BASE);
+QuickBooks.setOauthVersion('2.0');
+
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.svg')));
+
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ resave: false, saveUninitialized: false, secret: 'JMAccountingAPI' }));
 
-app.use('/', index);
-app.use('/users', users);
+app.get('/', (req, res) => res.render('pages/qbconnect'));
+app.get('/home', (req, res) => res.render('pages/home'));
+app.get('/error', (req, res) => res.render('pages/qbconnect-error'));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use('/api', qbconnect);
 
 module.exports = app;
---
+++ routes/qbconnect.js
@@ -0,0 +1,92 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var QuickBooks = require('node-quickbooks');
+var Tokens = require('csrf');
+var csrf = new Tokens();
+const util = require('util');
+
+// INSERT YOUR CONSUMER_KEY AND CONSUMER_SECRET HERE
+
+// SandBox
+// var consumerKey = 'Q0q63VIwq7SAQ8v6aop4mol1V6n0jk2lUB5WKu4LPN60wJLgBF';
+// var consumerSecret = 'gJaFcxJ8ouAx5RlQSkIjsjhwIUqfYXcP1IHaJeqC';
+
+// PRODUCTION  
+var consumerKey = 'Q0VDE5PpLWcqaT2sZHjjFYHakq2vB6OBqa67Uhm7JrlyOSzski';
+var consumerSecret = 'FbEwwsfQi6V5DRUbjVAApAYjjMPuuz1161ipI1jg';
+
+/* GET home page. */
+router.get('/requestToken', function (req, res, next) {
+
+    var redirecturl = QuickBooks.AUTHORIZATION_URL +
+        '?client_id=' + consumerKey +
+        '&redirect_uri=' + encodeURIComponent('https://janhavimeadow-api.herokuapp.com/api/callback') +  //Make sure this path matches entry in application dashboard
+        '&scope=com.intuit.quickbooks.accounting' +
+        '&response_type=code' +
+        '&state=' + generateAntiForgery(req.session);
+
+    res.redirect(redirecturl);
+
+});
+
+router.get('/callback', function (req, res, next) {
+    var auth = (new Buffer(consumerKey + ':' + consumerSecret).toString('base64'));
+    console.log(auth);
+
+    var postBody = {
+        url: 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: 'Basic ' + auth,
+        },
+        form: {
+            grant_type: 'authorization_code',
+            code: req.query.code,
+            redirect_uri: 'https://janhavimeadow-api.herokuapp.com/api/callback'  //Make sure this path matches entry in application dashboard
+        }
+    };
+
+    request.post(postBody, function (err, response, data) {
+
+        if (err || response.statusCode != 200) {
+            console.log("--Callback-- Error: " + err);
+            res.render('pages/qbconnect-error');
+        }
+
+        var accessToken = JSON.parse(response.body);
+
+        // save the access token somewhere on behalf of the logged in user
+        var qbo = new QuickBooks(consumerKey,
+            consumerSecret,
+            accessToken.access_token, /* oAuth access token */
+            false, /* no token secret for oAuth 2.0 */
+            req.query.realmId,
+            false, /* use a sandbox account */
+            true, /* turn debugging on */
+            14, /* minor version */
+            '2.0', /* oauth version */
+            accessToken.refresh_token /* refresh token */);
+
+        qbo.findCustomers(function (err, customers) {
+            if (err) {
+                console.log("Error: " + util.inspect(err));
+            }
+            else {
+                console.log("Ok: ");
+                console.log(customers.QueryResponse.Customer);
+            }
+        });
+
+    });
+    res.send('<!DOCTYPE html><html lang="en"><head></head><body><script>window.opener.location.assign("/home");window.close();</script></body></html>');
+});
+
+// OAUTH 2 makes use of redirect requests
+function generateAntiForgery(session) {
+    session.secret = csrf.secretSync();
+    return csrf.create(session.secret);
+};
+
+module.exports = router;
