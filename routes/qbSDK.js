53187573574c333d955d6f4d1fa62d6095bb4b13
Mukesh Selvaraj <33991311+ssmukesh@users.noreply.github.com>
2018-02-01 09:03:35 +0530

    prod
    Signed-off-by: Mukesh Selvaraj <33991311+ssmukesh@users.noreply.github.com>

--- routes/qbSDK.js
+++ routes/qbSDK.js
@@ -22,7 +22,7 @@ router.get('/getCompanyInfo', function (req, res, next) {
     var commonLogs = new CommonLogs();
     commonLogs = sessionManager.getCommonLogs(req.session);
 
-    var qbSDK = helper.getQuickBooksSDK(sessionManager.getQBConfig(req.session));    
+    var qbSDK = helper.getQuickBooksSDK(sessionManager.getQBConfig(req.session));
 
     if (qbSDK) {
 
@@ -38,6 +38,42 @@ router.get('/getCompanyInfo', function (req, res, next) {
                 sessionManager.saveCommonLogs(req.session, commonLogs);
                 var configCode = helper.getConfig_Codes("success", "QuickBooks", "1120");
                 return res.json({ status: configCode, QBOData: companyInfo, statusCode: 200 });
+            }
+        });
+
+    }
+    else {
+        commonLogs.validToken = false;
+        sessionManager.saveCommonLogs(req.session, commonLogs);
+        var configCode = helper.getConfig_Codes("error", "API", "0000");
+        return res.json({ status: configCode, statusCode: 200 });
+    }
+
+});
+
+router.get('/findCustomers', function (req, res, next) {
+
+    if (!req.session) { return res.json({ status: { statusType: "JMA-ST-151", error: null }, statusCode: 200 }); }
+
+    var commonLogs = new CommonLogs();
+    commonLogs = sessionManager.getCommonLogs(req.session);
+
+    var qbSDK = helper.getQuickBooksSDK(sessionManager.getQBConfig(req.session));
+
+    if (qbSDK) {
+
+        qbSDK.findCustomers(function (err, customers) {
+            if (err) {
+                commonLogs.validToken = false;
+                sessionManager.saveCommonLogs(req.session, commonLogs);
+                var configCode = helper.getConfig_Codes("error", "QuickBooks", "0001");
+                return res.json({ status: configCode, statusCode: 200 });
+            }
+            else {
+                commonLogs.validToken = true;
+                sessionManager.saveCommonLogs(req.session, commonLogs);
+                var configCode = helper.getConfig_Codes("success", "QuickBooks", "1130");
+                return res.json({ status: configCode, QBOData: customers.QueryResponse.Customer, statusCode: 200 });
             }
         });
 
